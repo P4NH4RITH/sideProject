813bef350528a3319bdb659263cc7d0a64b529f9
p4nh4rith <panharith.muong@gmail.com>
2022-06-03 09:14:06 +0200

    remove unused JS tag

--- JS/menu.js
+++ JS/menu.js
@@ -1,5 +1,6 @@
 let burger = document.querySelector('header');
 
 burger.addEventListener('click', function(){
-    burger.classList.toggle('menuBurger');    
-});   
+    burger.classList.toggle('menuBurger');
+    menu.style.cssText = "display: contents";
+});
